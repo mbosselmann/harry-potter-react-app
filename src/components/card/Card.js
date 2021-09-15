0761c340fb31839ef3342544656261ee846d793f
Mareike Boßelmann <mareike.bosselmann@gmx.de>
2021-09-15 17:36:56 +0200

    Add states for navbar, cards and emoji-tags

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import "./Card.css";
 import expandMore from "./expand-more.png";
+import expandLess from "./expand-less.png";
 
 function tellHouseColor(hogwartsHouse) {
   let houseColor;
@@ -17,6 +19,9 @@ function tellHouseColor(hogwartsHouse) {
 
 function Card(props) {
   const houseColor = tellHouseColor(props.hogwartsHouse);
+  // const showDetails = true;
+  const [showDetails, setShowDetails] = useState(false);
+  const [emoji, setEmoji] = useState("");
 
   return (
     <section className="section-card">
@@ -24,26 +29,96 @@ function Card(props) {
         <div>
           <img className="section-img" src={props.imageURL} alt="" />
         </div>
-        <h2 className="section-header-title">{props.characterName}</h2>
+        <h2 className="section-header-title">
+          {emoji}
+          {props.characterName}
+        </h2>
         <div className={houseColor}>{props.hogwartsHouse}</div>
-        <button className="expand-more-button">
-          <img className="expand-more-img" src={expandMore} alt="" />
+        <button
+          className={!showDetails ? "expand-more-button" : "expand-less-button"}
+          onClick={() => {
+            setShowDetails(!showDetails);
+          }}
+        >
+          <img
+            className="expand-more-img"
+            src={!showDetails ? expandMore : expandLess}
+            alt=""
+          />
         </button>
       </div>
-      <div className="section-content">
-        <ul className="main-info-list">
-          <h3>Basics:</h3>
-          <li>Gender: {props.gender}</li>
-          <li>Birthday: {props.birthday}</li>
-          <li>Patronus: {props.patronus}</li>
-        </ul>
-        <ul className="wand-list">
-          <h3>Wand:</h3>
-          <li>Wood: {props.wandWood}</li>
-          <li>Core: {props.wandCore}</li>
-          <li>Length: {props.wandLength}</li>
-        </ul>
-      </div>
+      {showDetails && (
+        <div className="section-content">
+          <div className="content-text">
+            <ul className="main-info-list">
+              <h3>Basics:</h3>
+              <li>Gender: {props.gender}</li>
+              <li>Birthday: {props.birthday}</li>
+              <li>Patronus: {props.patronus}</li>
+            </ul>
+            <ul className="wand-list">
+              <h3>Wand:</h3>
+              <li>Wood: {props.wandWood}</li>
+              <li>Core: {props.wandCore}</li>
+              <li>Length: {props.wandLength}</li>
+            </ul>
+            {props.characterName === "Harry Potter" ? (
+              <div className="emoji">⚡</div>
+            ) : (
+              <div></div>
+            )}
+            {props.characterName === "Hermione Granger" && (
+              <div className="emoji">📚</div>
+            )}
+            {props.characterName === "Ron Weasley" && (
+              <div className="emoji">🕸️</div>
+            )}
+            {props.characterName === "Minerva McGonagall" && (
+              <div className="emoji">🐱</div>
+            )}
+            {props.characterName === "Severus Snape" && (
+              <div className="emoji">🧪</div>
+            )}
+            {props.characterName === "Neville Longbottom" && (
+              <div className="emoji">⚔️</div>
+            )}
+          </div>
+          <div className="emoji-buttons">
+            <button
+              className="emoji-item"
+              onClick={() => {
+                setEmoji(!emoji ? "🔴  " : "");
+              }}
+            >
+              Target 🔴
+            </button>
+            <button
+              className="emoji-item"
+              onClick={() => {
+                setEmoji(!emoji ? "🔵  " : "");
+              }}
+            >
+              Catched 🔵
+            </button>
+            <button
+              className="emoji-item"
+              onClick={() => {
+                setEmoji(!emoji ? "🟣  " : "");
+              }}
+            >
+              Dead 🟣
+            </button>
+            <button
+              className="emoji-item"
+              onClick={() => {
+                setEmoji(!emoji ? "🟢  " : "");
+              }}
+            >
+              Ally 🟢
+            </button>
+          </div>
+        </div>
+      )}
     </section>
   );
 }
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,13 +1,60 @@
+import { useState } from "react";
 import "./Footer.css";
 
 function Footer() {
+  const [activeHouse, setActiveHouse] = useState("");
+
   return (
     <footer>
       <nav id="house-filter">
-        <button className="nav-button-gryffindor">G</button>
-        <button className="nav-button-ravenclaw">R</button>
-        <button className="nav-button-hufflepuff">H</button>
-        <button className="nav-button-slytherin">Allies</button>
+        <button
+          className={
+            activeHouse === "Gryffindor"
+              ? "nav-button-gryffindor--active"
+              : "nav-button-gryffindor"
+          }
+          onClick={() => {
+            setActiveHouse("Gryffindor");
+          }}
+        >
+          G
+        </button>
+        <button
+          className={
+            activeHouse === "Ravenclaw"
+              ? "nav-button-ravenclaw--active"
+              : "nav-button-ravenclaw"
+          }
+          onClick={() => {
+            setActiveHouse("Ravenclaw");
+          }}
+        >
+          R
+        </button>
+        <button
+          className={
+            activeHouse === "Hufflepuff"
+              ? "nav-button-hufflepuff--active"
+              : "nav-button-hufflepuff"
+          }
+          onClick={() => {
+            setActiveHouse("Hufflepuff");
+          }}
+        >
+          H
+        </button>
+        <button
+          className={
+            activeHouse === "Slytherin"
+              ? "nav-button-slytherin--active"
+              : "nav-button-slytherin"
+          }
+          onClick={() => {
+            setActiveHouse("Slytherin");
+          }}
+        >
+          Allies
+        </button>
       </nav>
     </footer>
   );
